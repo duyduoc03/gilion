54653f1f77d2823caf80b862203ecb07afe93d04
duyduoc03 <duyduoc03bg@gmail.com>
2022-11-29 22:10:17 +0700

    .

--- time.js
+++ time.js
@@ -1,14 +1,60 @@
-// back to top
+//time
+var time = new Date();
+nam = time.getFullYear();
+thang = time.getMonth();
+ngay = time.getDate();
+str = ngay + "/" + thang + "/" + nam;
+document.getElementById("date").innerHTML=str;
 
-window.onscroll = function() {scrollFunction()};
-          function scrollFunction() {
-            if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-            document.getElementById("myBtn").style.display = "block";
-            } else {
-            document.getElementById("myBtn").style.display = "none";
-            }
-            }
-          document.getElementById('myBtn').addEventListener("click", function(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-          });
+// back to top
+// window.onscroll = function() {scrollFunction()};
+//           function scrollFunction() {
+//             if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+//             document.getElementById("myBtn").style.display = "block";
+//             } else {
+//             document.getElementById("myBtn").style.display = "none";
+//             }
+//             }
+//           document.getElementById('myBtn').addEventListener("click", function(){
+//             document.body.scrollTop = 0;
+//             document.documentElement.scrollTop = 0;
+//           });
+
+//carousel1
+$('#carouselExampleInterval').on('slide.bs.carousel', function (e) {
+
+  
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 4;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
+$('#carouselExampleInterval').carousel({
+    interval: 1000
+});
+// $(document).ready(function() {
+//     /* show lightbox when clicking a thumbnail */
+//     $('div.panel').click(function(event){
+//         event.preventDefault();
+//         var content = $('.modal-body');
+//         content.empty();
+//         var title = $(this).attr("title");
+//         $('.modal-title').html(title);
+//         content.html($(this).html());
+//         $(".modal-profile").modal({show:true});
+//     });
+//
+// });
