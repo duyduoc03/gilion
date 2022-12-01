90f915ec590ab8cff40b5ffe1ac6aa5c91191f1f
duyduoc03 <duyduoc03bg@gmail.com>
2022-12-02 05:53:19 +0700

    tsg

--- time.js
+++ time.js
@@ -5,38 +5,4 @@ thang = time.getMonth()+1;
 ngay = time.getDate();
 document.getElementById("date").innerHTML=ngay + "/" + thang + "/" + nam;
 
-// back to top
-// window.onscroll = function() {scrollFunction()};
-//           function scrollFunction() {
-//             if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-//             document.getElementById("myBtn").style.display = "block";
-//             } else {
-//             document.getElementById("myBtn").style.display = "none";
-//             }
-//             }
-//           document.getElementById('myBtn').addEventListener("click", function(){
-//             document.body.scrollTop = 0;
-//             document.documentElement.scrollTop = 0;
-//           });
-
-//carousel1
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
-//   var $e = $(e.relatedTarget);
-//   var idx = $e.index();
-//   var itemsPerSlide = 4;
-//   var totalItems = $('.carousel-item').length;
-//
-//   if (idx >= totalItems-(itemsPerSlide-1)) {
-//       var it = itemsPerSlide - (totalItems - idx);
-//       for (var i=0; i<it; i++) {
-//           // append slides to end
-//           if (e.direction==="left") {
-//               $('.carousel-item').eq(i).appendTo('.carousel-inner');
-//           }
-//           else {
-//               $('.carousel-item').eq(0).appendTo('.carousel-inner');
-//           }
-//       }
-//   }
-// });
 
