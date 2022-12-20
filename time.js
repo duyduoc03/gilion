a650807d39dbbd1d2b79409a1fa9914d9980ef5c
duyduoc03 <duyduoc03bg@gmail.com>
2022-12-20 14:41:16 +0700

    ht

--- time.js
+++ time.js
@@ -1,6 +1,5 @@
-//time
 var time = new Date();
 nam = time.getFullYear();
 thang = time.getMonth()+1;
 ngay = time.getDate();
-document.getElementById("date").innerHTML=ngay + "/" + thang + "/" + nam;
+document.getElementById("date").innerHTML=(ngay + "/" + thang + "/" + nam);
